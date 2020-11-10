e4ce00161cd3bea3d325f1b8e57c54a49276fb2e
Rustam <punk60826@gmail.com>
2020-11-10 02:58:30 +0200

    finished project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 
 import 'semantic-ui-react';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -15,14 +16,23 @@ componentDidMount(){
         
     );
 }
+
+renderContent() {
+  if(this.state.errorMessage && !this.state.lat){
+    return <div> Error: {this.state.errorMessage}</div>
+  }
+  if(!this.state.errorMessage && this.state.lat){
+    return <SeasonDisplay lat={this.state.lat} />
+  }
+ return <Spinner message="Please accept location request" />;
+}
+
   render(){
-      if(this.state.errorMessage && !this.state.lat){
-        return <div> Error: {this.state.errorMessage}</div>
-      }
-      if(!this.state.errorMessage && this.state.lat){
-        return <SeasonDisplay lat={this.state.lat} />
-      }
-     return <div>Loading! </div>
+     return (
+       <div className="border red">
+            {this.renderContent()}
+        </div> 
+     )
   }
 
 }
